6f077b7d5aee7bccb4934f27a07bbb1bbbb62108
admin <admin@La40020994.local>
2026-04-21 09:55:23 +0300

    add origin for vercel

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173',
+      'https://todo-app-sigma-green.vercel.app'
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
